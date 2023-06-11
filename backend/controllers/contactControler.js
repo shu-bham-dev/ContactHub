ab586cdaa11bdc38fd8ce16073e0b069feb48c93
Shubham Sahu <ss.cse1001@gmail.com>
2023-06-11 17:34:59 +0530

    add crud operation to schema

--- backend/controllers/contactControler.js
+++ backend/controllers/contactControler.js
@@ -1,29 +1,48 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require('express-async-handler')
+const contactSchema = require('../models/contactModel')
+const { mongoose } = require('mongoose')
 
-const getContacts = (req,res) =>{
-    res.status(200).json({message:"Get all contacts"})
+const getContacts = async (req, res) => {
+    const Contacts = await contactSchema.find()
+    res.status(200).json(Contacts)
 }
 
-const postContacts =asyncHandler(async (req,res)=>{
-    console.log(req.body);
-    const {name,email,phone} = req.body;
-    if(!name || !email || !phone){
-        res.status(400);
-        throw new Error("All fields must be present");
+const postContacts = asyncHandler(async (req, res) => {
+    console.log(req.body)
+    const { name, email, phone } = req.body
+    if (!name || !email || !phone) {
+        res.status(400)
+        throw new Error('All fields must be present')
     }
-    res.status(200).json({message: "Create contacts"})
+    const contact = await contactSchema.create({
+        name,
+        phone,
+        email,
+    })
+    res.status(200).json(contact)
 })
 
-const getContactByid = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Create contacts for ${req.params.id}`})
+const getContactByid = asyncHandler(async (req, res) => {
+    const contactByID = await contactSchema.findById(req.params.id)
+    if (!contactByID) {
+        res.status(404)
+        throw new Error('Contact not found')
+    }
+    res.status(200).json(contactByID)
 })
 
-const updateContact = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Update contacts for ${req.params.id}`})
+const updateContact = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update contacts for ${req.params.id}` })
 })
 
-const deleteContact = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Delete contacts for ${req.params.id}`})
+const deleteContact = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete contacts for ${req.params.id}` })
 })
 
-module.exports = {deleteContact, updateContact,getContactByid, postContacts,getContacts}
+module.exports = {
+    deleteContact,
+    updateContact,
+    getContactByid,
+    postContacts,
+    getContacts,
+}
