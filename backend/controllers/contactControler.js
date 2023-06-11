546f99611ba05722fb78d3f1608b4743b6584d0d
Shubham Sahu <ss.cse1001@gmail.com>
2023-06-11 15:06:37 +0530

    add express async handler

--- backend/controllers/contactControler.js
+++ backend/controllers/contactControler.js
@@ -1,8 +1,10 @@
+const asyncHandler = require('express-async-handler');
+
 const getContacts = (req,res) =>{
     res.status(200).json({message:"Get all contacts"})
 }
 
-const postContacts = (req,res)=>{
+const postContacts =asyncHandler(async (req,res)=>{
     console.log(req.body);
     const {name,email,phone} = req.body;
     if(!name || !email || !phone){
@@ -10,18 +12,18 @@ const postContacts = (req,res)=>{
         throw new Error("All fields must be present");
     }
     res.status(200).json({message: "Create contacts"})
-}
+})
 
-const getContactByid = (req,res)=>{
+const getContactByid = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `Create contacts for ${req.params.id}`})
-}
+})
 
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `Update contacts for ${req.params.id}`})
-}
+})
 
-const deleteContact = (req,res)=>{
+const deleteContact = asyncHandler(async (req,res)=>{
     res.status(200).json({message: `Delete contacts for ${req.params.id}`})
-}
+})
 
 module.exports = {deleteContact, updateContact,getContactByid, postContacts,getContacts}
